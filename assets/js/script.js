c992e769e12a0d4394a01969b4e0e3c3e8984961
Pascal Harting <pascalharting98@gmail.com>
2024-05-16 17:43:49 +0000

    Fix paths to HTML documents because of GitHub error again

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,9 @@ const mainContainer = document.getElementsByTagName('main')[0];
 // Checks if username is stored, if not loads login.html
 document.addEventListener('DOMContentLoaded', function() {
     if (localStorage.getItem('username')) {
-        loadContent("../main.html");
+        loadContent("main.html");
     } else {
-        loadContent("../login.html");
+        loadContent("login.html");
     }
 });
 
@@ -17,14 +17,14 @@ async function loadContent(page) {
     let content = await response.text();
     mainContainer.innerHTML = content;
 
-    if(page === "../login.html") {
+    if(page === "login.html") {
         document.getElementById('login-form').addEventListener('submit', function(event) {
             event.preventDefault();
             let username = document.getElementById('username').value;
             let goal = document.getElementById('goal').value;
             localStorage.setItem("username", username);
             localStorage.setItem("goal", goal);
-            loadContent("../main.html");
+            loadContent("main.html");
         });
     }
 }
